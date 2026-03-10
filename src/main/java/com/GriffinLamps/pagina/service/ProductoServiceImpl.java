2ff45dcdd154ee462883ccc2ee6b6279b2f4641f
Pazy03 <159201969+Pazy03@users.noreply.github.com>
2026-03-09 20:24:58 -0600

    Create ProductoServiceImpl.java

---
+++ src/main/java/com/GriffinLamps/pagina/service/ProductoServiceImpl.java
@@ -0,0 +1,34 @@
+package com.GriffinLamps.pagina.service;
+
+import com.GriffinLamps.pagina.domain.Producto;
+import com.GriffinLamps.pagina.repository.ProductoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public List<Producto> getProductos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public Producto getProducto(Producto producto) {
+        return productoRepository.findById(producto.getIdProducto()).orElse(null);
+    }
+
+    @Override
+    public void save(Producto producto) {
+        productoRepository.save(producto);
+    }
+
+    @Override
+    public void delete(Producto producto) {
+        productoRepository.delete(producto);
+    }
+}
