c9cc3745f75055bdce42848103ebcf37d6ac8f34
Pazy03 <159201969+Pazy03@users.noreply.github.com>
2026-03-09 20:25:45 -0600

    Create ProductoController.java

---
+++ src/main/java/com/GriffinLamps/pagina/controller/ProductoController.java
@@ -0,0 +1,46 @@
+package com.GriffinLamps.pagina.controller;
+
+import com.GriffinLamps.pagina.domain.Producto;
+import com.GriffinLamps.pagina.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/producto")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        model.addAttribute("productos", productoService.getProductos());
+        return "/producto/listado";
+    }
+
+    @GetMapping("/nuevo")
+    public String productoNuevo(Producto producto) {
+        return "/producto/modifica";
+    }
+
+    @PostMapping("/guardar")
+    public String productoGuardar(Producto producto) {
+        productoService.save(producto);
+        return "redirect:/producto/listado";
+    }
+
+    @GetMapping("/eliminar/{idProducto}")
+    public String productoEliminar(Producto producto) {
+        productoService.delete(producto);
+        return "redirect:/producto/listado";
+    }
+
+    @GetMapping("/modificar/{idProducto}")
+    public String productoModificar(Producto producto, Model model) {
+        producto = productoService.getProducto(producto);
+        model.addAttribute("producto", producto);
+        return "/producto/modifica";
+    }
+}
