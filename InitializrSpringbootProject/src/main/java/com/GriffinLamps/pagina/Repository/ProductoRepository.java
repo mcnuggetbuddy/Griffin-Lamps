7aa21554fe52197de3707087490ad0a694cd0439
Pazy03 <159201969+Pazy03@users.noreply.github.com>
2026-03-10 19:11:36 -0600

    Actualizar ProductoRepository

--- InitializrSpringbootProject/src/main/java/com/GriffinLamps/pagina/Repository/ProductoRepository.java
+++ InitializrSpringbootProject/src/main/java/com/GriffinLamps/pagina/Repository/ProductoRepository.java
@@ -3,6 +3,6 @@ package com.GriffinLamps.pagina.Repository;
 import com.GriffinLamps.pagina.Domain.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 
 }
