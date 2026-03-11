2955281cc76ddc634e796e9bad042ea836f6c267
mcnuggetbuddy <apuyisrael@gmail.com>
2026-03-11 00:59:36 -0600

    Creación de About
    Se crearon los templates para About
    Se modificaron los messages.properties, estilos.css

---
+++ InitializrSpringbootProject/src/main/java/com/GriffinLamps/pagina/Controller/AboutController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.GriffinLamps.pagina.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/about")
+public class AboutController {
+
+    @GetMapping("/listado")
+    public String listado() {
+        return "about/listado";
+    }
+
+}
