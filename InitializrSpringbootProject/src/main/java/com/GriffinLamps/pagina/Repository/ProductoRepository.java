c0ce6f404cfd2620abae0ae296bfe342933e33f2
mcnuggetbuddy <apuyisrael@gmail.com>
2026-03-11 23:10:39 -0600

    Modificación Producto
    Se modifica producto

--- InitializrSpringbootProject/src/main/java/com/GriffinLamps/pagina/Repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.GriffinLamps.pagina.Repository;
-
-import com.GriffinLamps.pagina.Domain.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-
-}
